852e0e348417ba12d33c1c61b4ad5c921ed8eeb8
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-08-01 15:58:07 +0200

    Add geo locator system

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
-class LoadingScreen extends StatelessWidget {
-  const LoadingScreen({Key? key}) : super(key: key);
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  void getPermission() async {
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.deniedForever) {
+        return Future.error('Location Not Available');
+      }
+    }
+  }
+
+  void getLocation() async {
+    getPermission();
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    print(position);
+
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: CircularProgressIndicator(),
+        child: ElevatedButton(
+          onPressed: () {
+            getLocation();
+          },
+          child: const Text('Get Location'),
+        ),
       ),
     );
   }
