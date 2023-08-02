e6b183bb42f7b8c8b5e5e8de97fb0d6374410216
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-08-02 12:35:54 +0200

    Set up location service

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -10,28 +10,9 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
-  void getPermission() async {
-    LocationPermission permission;
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.deniedForever) {
-        return Future.error('Location Not Available');
-      }
-    }
-  }
-
   void getLocation() async {
-    getPermission();
-
-    try {
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.low);
-      print(position);
-    } catch (e) {
-      print(e);
-    }
-
+    Location location = Location();
+    await location.getCurrentLocation();
   }
 
   @override
---
+++ lib/services/location.dart
@@ -0,0 +1,28 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double? latitude;
+  double? longitude;
+
+  Future<void> getCurrentLocation() async {
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.deniedForever) {
+        return Future.error('Location Not Available');
+      }
+    }
+
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      latitude = position.latitude;
+      longitude = position.longitude;
+      print('latitude: $latitude, longitude: $longitude');
+    } catch (e) {
+      print(e);
+    }
+  }
+
+}
